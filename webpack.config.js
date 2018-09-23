cf581f7e99c2ce05eca00fac4f0d775eb80a37b1
Kwon <kvertzizis@gmail.com>
2018-09-23 12:03:03 +0100

    mobile nav button progress

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: "./app/assets/scripts/App.js",
+  output: {
+    path: path.resolve(__dirname, "./app/temp/scripts"),
+    filename: "App.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015']
+          }
+        }
+      }
+    ]
+  }
+}
